20f38e75ea025b9d669e58f8381d1c444e4c1cd9
ngopal561998 <ngopal561998@gmail.com>
2021-06-12 20:45:52 +0530

    kahv

--- src/App.js
+++ src/App.js
@@ -4,15 +4,23 @@ import React from "react";
 import {BrowserRouter as Router,Switch,Route} from "react-router-dom";
 
 
+import { LinkedInPopUp } from 'react-linkedin-login-oauth2';
+import LinkedInPage from './Screen/LinkedInPage'
+
 
 export default function BasicExample() {
   return (
     <Router>
-        <Switch>
+        {/* <Switch>
           <Route exact path="/">
             <Home />
           </Route>
-        </Switch>
+        </Switch> */}
+
+        <Switch >
+        <Route exact path="/linkedin" component={LinkedInPopUp} />
+        <Route path="/" component={LinkedInPage} />
+      </Switch>
     </Router>
   );
 }
---
+++ src/Screen/LinkedInPage.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+
+import { LinkedIn } from 'react-linkedin-login-oauth2';
+import linkedin from 'react-linkedin-login-oauth2/assets/linkedin.png'
+
+class LinkedInPage extends Component {
+  state = {
+    code: '',
+    errorMessage: '',
+  };
+
+
+  handleSuccess = (data) => {
+    console.log("data", data)
+    this.setState({
+      code: data.code,
+      errorMessage: '',
+    });
+  }
+
+  handleFailure = (error) => {
+    this.setState({
+      code: '',
+      errorMessage: error.errorMessage,
+    });
+  }
+  render() {
+    const { code, errorMessage } = this.state;
+    return (
+      <div>
+        <LinkedIn
+          clientId="86bf5uhj67ssy0"
+          redirectUri={`${window.location.origin}/linkedin`}
+          scope="r_emailaddress"
+          state="34232423"
+          onFailure={this.handleFailure}
+          onSuccess={this.handleSuccess}
+          supportIE
+          redirectPath='/linkedin'
+        >
+          <img src={linkedin} alt="Log in with Linked In" style={{ maxWidth: '180px' }} />
+        </LinkedIn>
+        {!code && <div>No code</div>}
+        {code && <div>Code: {code}</div>}
+        {errorMessage && <div>{errorMessage}</div>}
+      </div>
+    );
+  }
+}
+
+export default LinkedInPage;
